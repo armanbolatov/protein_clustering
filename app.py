6e7ac64ecf61860d23901d6649cf4618d8c52ecc
Arman Bolatov <a.bol@bc-pf.org>
2023-10-11 00:06:58 +0600

    bug fixes

--- app.py
+++ app.py
@@ -5,8 +5,24 @@ import json
 
 PATH = "plots/"
 
+st.set_page_config(
+    page_title="Protein-Ligand Interaction Visualizer",
+    page_icon="🧬",
+    initial_sidebar_state="expanded",
+)
+st.title("Protein-Ligand Interaction Visualizer 🧬")
+st.markdown("""
+Welcome to the Protein-Ligand Interaction Visualizer. This application provides a 3D visualization of protein-ligand interactions based on various parameters (`pKi`, `pKd`, `pIC50`, `pEC50`). 
+
+The visualizations are based on protein embeddings obtained from a pre-trained model, specifically [ProtBERT](https://huggingface.co/Rostlab/prot_bert). The embeddings are reduced to three dimensions using [UMAP](https://umap-learn.readthedocs.io/en/latest/).
+
+The application allows you to select a specific ligand and a parameter of interest from the dropdown menus. The corresponding 3D scatter plot generated using Plotly is then displayed, with each point representing a protein. The color of the points indicates the value of the selected parameter for the interaction between the protein and the ligand.
+
+The data used in this application comes from [BindingDB](https://www.bindingdb.org/bind/index.jsp).
+""")
+
 parameter = st.selectbox(
-    "Parameters",
+    "Parameter",
     os.listdir(PATH)
 )
 
